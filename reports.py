5382559ad45933bc50c1ccb0b8d882e1210688fe
maxiiii46 <maximoleguizamon428@gmail.com>
2026-06-19 16:56:09 -0300

    Delete reports.py

--- reports.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from models import Order
-
-app = Flask(__name__)
-
-@app.route('/reports')
-def reports():
-    total_orders = Order.query.count()
-    return f"Total de pedidos: {total_orders}"
