68fa522f70637553f6f31f62a0783e9fe4720a36
maxiiii46 <maximoleguizamon428@gmail.com>
2026-06-19 13:54:29 -0300

    Move init_db call outside of main guard

--- app.py
+++ app.py
@@ -336,7 +336,6 @@ def admin_dashboard():
 # ──────────────────────────────────────────────
 
 # LLAMADA OBLIGATORIA FUERA DEL IF PARA RENDER
-init_db() 
-
 if __name__ == "__main__":
+    init_db()
     app.run(debug=True)
